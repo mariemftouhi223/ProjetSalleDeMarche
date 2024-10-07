827083429af1a7240799e3f1dcc30d5bf61a3918
linda <khiari.linda@esprit.tn>
2024-10-07 16:04:00 +0100

    Message décrivant les modifications

---
+++ ProjetSalleDeMarche-main/src/main/java/tn/esprit/projetsalledemarche/Controller/EvenementController.java
@@ -0,0 +1,6 @@
+package tn.esprit.projetsalledemarche.Controller;
+
+public class EvenementController {
+
+
+}
---
+++ ProjetSalleDeMarche-main/src/main/java/tn/esprit/projetsalledemarche/Entity/Sinistre.java
@@ -0,0 +1,4 @@
+package tn.esprit.projetsalledemarche.Entity;
+
+public class Sinistre {
+}
