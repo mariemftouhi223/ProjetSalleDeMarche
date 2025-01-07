9b2ff05277d39b8fcd94c1e8330cdf941c4e6e67
mehdikhallef <mehdi.khallef@esprit.tn>
2025-01-07 21:48:12 +0100

    merged souha

--- src/main/java/tn/esprit/projetsalledemarche/Repository/autrerepo/SinistreRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.projetsalledemarche.Repository.autrerepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.projetsalledemarche.Entity.autre.Sinistre;
-
-public interface SinistreRepository extends JpaRepository<Sinistre,Long> {
-}
